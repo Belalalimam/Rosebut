13502a80c28b02cb5c345792d823aaf44a3f3912
Belalalimam <bb5503344@gmail.com>
2024-02-22 01:49:17 +0200

    Rosebu

---
+++ main.js
@@ -0,0 +1,124 @@
+
+
+// function toCelsius(f) {
+//   return (5/9) * (f-32);
+// }
+
+// let value = toCelsius(77);
+// document.getElementById("demo").innerHTML = value;
+
+
+
+var arr = ["anas", "belal", "marwa", "me"]
+var text = "";
+
+// for(var i = 0; i < arr.length; i++){
+// text += arr[i] + "<br>";
+// }
+// document.getElementById("demo").innerHTML = text ;
+
+// const g = new Date();
+// g.setTime(1332403882588);
+// document.getElementById("demo").innerHTML = g;
+
+
+
+
+
+
+
+// clock
+
+// function mytime(){
+
+// var date = new Date();
+
+// var houresss = date.getHours();
+// var minutes = date.getMinutes();
+// var seconds = date.getSeconds();
+
+// houresss = (houresss < 10) ? "0" + houresss : houresss;
+// minutes = (minutes < 10) ? "0" + minutes : minutes;
+// seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+// if (houresss === 0){
+// houresss = 12;
+// }
+// if (houresss > 12){
+//     houresss -= 12;
+// }
+// var time = houresss + ":" + minutes + ":" + seconds;
+
+// document.getElementById("demo").innerHTML = time;
+
+// setTimeout(mytime, 1000)
+
+// }
+// mytime();
+
+
+
+
+
+// date
+
+// const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+// const d = new Date();
+// document.getElementById("demo").innerHTML = days[d.getDay()];
+
+
+
+
+// var text = "Outside: " + typeof carName;
+//   document.getElementById("demo1").innerHTML = text;
+  
+//   function myFunction() {
+//     var carName = "Volvo";
+//     var text = "Inside: " + typeof carName + "  " + carName; 
+//     document.getElementById("demo2").innerHTML = text;
+//   }
+  
+//   myFunction();
+
+
+
+
+
+
+// app
+
+// let text;
+//     const today = new Date();
+//     const someday = new Date();
+//     someday.setFullYear(2010, 0, 14);  
+//     if (someday > today) {
+//       text = "Today is before January 14, 2100.";
+//     } else {
+//       text = "Today is after January 14, 2100.";
+//     }
+//     document.getElementById("demo").innerHTML = text;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+// let len = cars.length;
+// let text = "";
+// for (let i = 0; i < len; i++) {
+//   text += cars[i] + "<br>";
+// }
+
+// document.getElementById("demo").innerHTML = text;
